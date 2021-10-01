9f60bb2d19a00b4f19de86feacbfae279aef2c07
adriacos <acosdc@gmail.com>
2021-10-01 18:22:19 +0200

    Blank ionic vue application

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'icc_maps',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
